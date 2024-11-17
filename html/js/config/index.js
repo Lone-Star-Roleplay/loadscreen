09afb1b16375f529c498569e8f8da2efc870197b
James <imblazeitjames@gmail.com>
2024-11-17 15:27:30 -0600

    Refactor audio controls and update dev variable handling; remove unused custom events

--- html/js/config/index.js
+++ html/js/config/index.js
@@ -5,6 +5,7 @@ import { configBackgroundMusic } from './background-music.js';
 import { configBackgroundVideos } from './background-videos.js';
 import { configCustomEvents, setupCustomEvents } from './custom-events.js';
 import { configErrorLog, setupErrorLog } from './error-log.js';
+import { configGenerator } from './generator.js';
 import { configMessages } from './messages.js';
 import { configStyle, setupStyle } from './style.js';
 
@@ -28,4 +29,5 @@ export function config(handoverData) {
     configBackgroundEmbed(handoverData);
     configBackgroundMusic(handoverData);
     configAudioControls(handoverData);
+    configGenerator(handoverData);
 }
